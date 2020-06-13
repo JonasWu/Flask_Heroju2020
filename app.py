b54cf566764c2a38b7a35dea0fb78d08c6ac92ca
JonasWu <m94g0101@stust.edu.tw>
2020-06-13 17:10:38 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -12,10 +12,10 @@ from linebot.models import *
 app = Flask(__name__)
 
 # Channel access token (long-lived)
-line_bot_api = LineBotApi('zjR9F8RZnez9urIOhACAN143CUoBoLxs/9MYyVKgrGzoY34o/fAVyP+xbHsLZ4aknPagK5K7Uub/VwE++irRyx5eC/rVojAi+VbXynT3egRtejn85FKBJJPG8ZZx6xFnD9Ywi//I82S7lZg5i/MfDAdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi('PPI/kYjsolx9bwKsFvYb/0Do/8Oa1aubTMoz95Vrf1YVY9ZsglojcW+o63pdEiUtnPagK5K7Uub/VwE++irRyx5eC/rVojAi+VbXynT3egSPi0UNpdCttjLvgy9gs7G7cRaj8FQcqFDZ9t9vWlUj9AdB04t89/1O/w1cDnyilFU=')
 
 # Channel secret 
-handler = WebhookHandler('133671cf26d684faf93b65d6f423d572')
+handler = WebhookHandler('c6fd22d25b991e2bb231121a249b3003')
 
 
 @app.route("/callback", methods=['POST'])
@@ -36,41 +36,36 @@ def callback():
     return 'OK'
 
 def getNews():
-    """
-    建立一個抓最新消息的function
-    """
-    import requests
-    import re
-    from bs4 import BeautifulSoup
+	"""
+	建立一個抓最新消息的function
+	"""
+	import requests
+	import re
+	from bs4 import BeautifulSoup
 
-    url = 'https://www.ettoday.net/news/focus/3C%E5%AE%B6%E9%9B%BB/'
-    r = requests.get(url)
-    reponse = r.text
+	url = 'https://www.ettoday.net/news/focus/3C%E5%AE%B6%E9%9B%BB/'
+	r = requests.get(url)
+	reponse = r.text
 
-    url_list = re.findall(r'<h3><a href="/news/[\d]*/[\d]*.htm" .*>.*</a>',reponse)
+	url_list = re.findall(r'<h3><a href="/news/[\d]*/[\d]*.htm" .*>.*</a>',reponse)
 
-    soup = BeautifulSoup(url_list[0])
-    url = 'https://fashion.ettoday.net/' + soup.find('a')['href']
-    title = soup.text
+	soup = BeautifulSoup(url_list[0])
+	url = 'https://fashion.ettoday.net/' + soup.find('a')['href']
+	title = soup.text
 
 
-    tmp = title + ': ' +url
-    return tmp
-
+	tmp = title + ': ' +url
+	return tmp
+	
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     # 傳送文字
     if event.message.text == '傳送文字':
         message = TextSendMessage(getNews())
-    # 傳送圖片
-    elif event.message.text == '傳送圖片':
-        message = ImageSendMessage(
-            original_content_url='https://i.imgur.com/QPJ8A1b.png',
-            preview_image_url='https://i.imgur.com/QPJ8A1b.png'
-        )
-    else:
+	else:
         message = TextSendMessage(text=event.message.text)
-        line_bot_api.reply_message(event.reply_token, message)
+    line_bot_api.reply_message(event.reply_token, message)
 
+	
 if __name__ == '__main__':
     app.run(debug=True)
